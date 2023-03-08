ad84aa6c3053dd10381627cab18dd42c9f58a233
Mikhail Marychev <mmarychev@kazanworkout.ru>
2023-03-08 22:21:00 +0300

    placed game field

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,46 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<nav>
+    <h1>
+        Trouble in Miami
+    </h1>
+</nav>
+<div id="game"></div>
+
+<style lang="scss">
+    :global(body) {
+        padding: 0;
+        margin: 0;
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+    }
+
+    nav {
+        height: 50px;
+        width: 100%;
+        h1 {
+            margin: 0;
+        }
+    }
+
+    #game {
+        width: 100%;
+        height: calc(100% - 50px);
+        overflow: hidden;
+    }
+</style>
+
+<script lang="ts">
+
+    import { onMount } from 'svelte';
+    import TroubleInMiami from '../lib';
+
+    onMount(() => {
+
+        const gameElement = document.getElementById("game") as HTMLElement;
+        new TroubleInMiami(gameElement);
+    })
+
+    
+</script>
